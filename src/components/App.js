047f1e78c69ff0d47ee568d1d893c5e0dabf634d
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-04-24 22:26:43 +0300

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from './About'
+import Article from './Article'
+import ArticleList from './ArticleList'
+import Header from './Header'
 
 console.log(blogData);
 
